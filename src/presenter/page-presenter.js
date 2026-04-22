936a4be60588a63a8c0c08f661b28927076e9d96
Lowder <me@lowderplay.dev>
2026-04-22 17:08:07 +0500

    refactor: split presenters

--- src/presenter/page-presenter.js
+++ src/presenter/page-presenter.js
@@ -1,55 +1,28 @@
-import EventsView from '../view/events-view';
 import InfoView from '../view/info-view';
 import FilterView from '../view/filter-view';
-import SortView from '../view/sort-view';
-import {render, RenderPosition, remove} from '../framework/render';
-import {EventPresenter} from './event-presenter';
-import EmptyMessageView from '../view/empty-message-view';
-
-const SortType = {
-  DAY: 'day',
-  TIME: 'time',
-  PRICE: 'price',
-};
-
-const sortEventsByDay = (eventA, eventB) =>
-  new Date(eventA.start).getTime() - new Date(eventB.start).getTime();
-
-const sortEventsByTime = (eventA, eventB) =>
-  (new Date(eventB.end).getTime() - new Date(eventB.start).getTime()) -
-  (new Date(eventA.end).getTime() - new Date(eventA.start).getTime());
-
-const sortEventsByPrice = (eventA, eventB) => eventB.price - eventA.price;
+import {render, RenderPosition} from '../framework/render';
+import {RoutePresenter} from './route-presenter';
 
 export class PagePresenter {
   #eventsModel;
-  #offersModel;
-  #destinationsModel;
-  #currentSortType = SortType.DAY;
-  #eventPresenters = {};
-  #eventsView = null;
-  #sortView = null;
+  #routePresenter;
 
   constructor({ eventsModel, offersModel, destinationsModel }) {
     this.#eventsModel = eventsModel;
-    this.#offersModel = offersModel;
-    this.#destinationsModel = destinationsModel;
+    this.#routePresenter = new RoutePresenter({
+      eventsModel,
+      offersModel,
+      destinationsModel,
+    });
   }
 
   init() {
-    this.#eventsView = new EventsView();
-
     if (this.#eventsModel.getEvents().length > 0) {
       this.#renderInfo();
-      this.#renderSort();
-    } else {
-      const message = new EmptyMessageView();
-      render(message, this.#eventsView.element);
     }
 
     this.#renderFilter();
-    this.#renderEvents();
-    this.#renderEventForms();
+    this.#routePresenter.init();
   }
 
   #renderInfo() {
@@ -71,80 +44,4 @@ export class PagePresenter {
 
     render(filterView, document.querySelector('.trip-controls__filters'));
   }
-
-  #renderSort() {
-    this.#sortView = new SortView(this.#currentSortType, this.#handleSortTypeChange);
-
-    render(
-      this.#sortView,
-      document.querySelector('.trip-events'),
-      RenderPosition.AFTERBEGIN,
-    );
-  }
-
-  #renderEvents() {
-    const events = this.#getSortedEvents();
-
-    for (const event of events) {
-      const presenter = new EventPresenter({
-        offersModel: this.#offersModel,
-        destinationsModel: this.#destinationsModel,
-        onDataChange: this.#handleEventChange,
-        onModeChange: this.#handleModeChange,
-      });
-
-      presenter.init(this.#eventsView, event);
-      this.#eventPresenters[event.id] = presenter;
-    }
-  }
-
-  #renderEventForms() {
-    render(this.#eventsView, document.querySelector('.trip-events'));
-  }
-
-  #clearEventsList() {
-    for (const key in this.#eventPresenters) {
-      this.#eventPresenters[key].destroy();
-    }
-    this.#eventPresenters = {};
-  }
-
-  #getSortedEvents() {
-    const events = [...this.#eventsModel.getEvents()];
-
-    switch (this.#currentSortType) {
-      case SortType.TIME:
-        return events.sort(sortEventsByTime);
-      case SortType.PRICE:
-        return events.sort(sortEventsByPrice);
-      case SortType.DAY:
-      default:
-        return events.sort(sortEventsByDay);
-    }
-  }
-
-  #handleSortTypeChange = (sortType) => {
-    if (this.#currentSortType === sortType) {
-      return;
-    }
-
-    this.#currentSortType = sortType;
-    this.#clearEventsList();
-    remove(this.#sortView);
-    this.#renderSort();
-    this.#renderEvents();
-  };
-
-  #handleEventChange = (updatedEvent) => {
-    this.#eventsModel.updateEvent(updatedEvent);
-
-    const presenter = this.#eventPresenters[updatedEvent.id];
-    presenter?.init(this.#eventsView, updatedEvent);
-  };
-
-  #handleModeChange = () => {
-    Object.values(this.#eventPresenters).forEach((presenter) =>
-      presenter.resetView(),
-    );
-  };
 }
